a7df33a801bca0248db216209a13d7d4586f457c
Adena McGibbon <justadena@gmail.com>
2019-09-29 16:51:23 -0600

    The code returns a string of random characters but does not match the user specified lenght.  Saving this just in case I break my code in the process of fixing this issue.

--- script.js
+++ script.js
@@ -11,42 +11,54 @@
 //Based on the above criteria return a string 
 
 
-
+// These are my global variables. I set up my arrays a little bit differently
 var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-var pwdLetters = alpha.split('');
-var upperLetters = alpha.slice(0,26);
-var lowerAlpha = pwdLetters.slice(26,52);
+var lettersArray = alpha.split('');
+var upperAlpha = alpha.slice(0,26);
+var lowerAlpha = lettersArray.slice(26,52);
 var symbols = '!@#$%^&*)?(';
 var specChars = symbols.split('');
 var userNeeds = prompt("How long should you password be?");
 var userNeedsSpec = confirm("Would you like to include special characters?");
-var includeNumber = confirm("Would you like to include numbers in your password?");
-// var upperCase = confirm("Would you like to include uppercase letters in your password?");
-// var lowerCase = confirm("Would you like to include lowercase letters in your password?");
+var userNeedsNum = confirm("Would you like to include numbers in your password?");
+var upperCase = confirm("Would you like to include uppercase letters in your password?");
+var lowerCase = confirm("Would you like to include lowercase letters in your password?");
 
-var randomNumber = Math.floor(Math.random() * pwdLetters.length);
+// var randomNumber = Math.floor(Math.random() * pwdLetters.length);
+
 var passWord = "" 
 
+
 for (i = 0; i < userNeeds; i++) {
-  var randomNumber = Math.floor(Math.random() * pwdLetters.length);  
+  var randomNumber = Math.floor(Math.random() * lettersArray.length);  
   var includeRandomNum = Math.floor(Math.random() * 10);
   var includeSpecChars = Math.floor(Math.random() * specChars.length);
+  var includeUpperCase = Math.floor(Math.random() * upperAlpha.length);
+  var includeLowerCase = Math.floor(Math.random() * lowerAlpha.length);
+
   if (passWord.length >= userNeeds) {
     break;
   }
+
   if (userNeedsSpec == true) {
     passWord += specChars[includeSpecChars];
       } 
-  if (includeNumber){
+  
+  if (userNeedsNum){
     passWord += includeRandomNum; 
   }  
+
+  if (upperCase) {
+    passWord += includeUpperCase;
+  }
+
   passWord += alpha[randomNumber];
   // the above line of code is shorthand for the below line of code
   // passWord = passWord + alpha[randomNumber];
    
 }
 console.log(passWord);
-
+console.log(passWord.length);
 
 
 // for (i = 0; i < userNeeds; i++) {
