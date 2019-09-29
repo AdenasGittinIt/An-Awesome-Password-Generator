51be8fdd5b5772ac3c8bc282b5e4c3effc980b04
Adena McGibbon <justadena@gmail.com>
2019-09-29 14:55:56 -0600

    Corrected an issue with duplicate letters.

--- script.js
+++ script.js
@@ -8,41 +8,47 @@
 
 //Capture whether the user wants to include upper characters
 
-//Return a string based on the above criteria return a string 
-
-
+//Based on the above criteria return a string 
 
 
 
 var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 var pwdLetters = alpha.split('');
-var specialChars = '!@#$%^&*)?(';
-var pwdChars = specialChars.split('');
+var upperLetters = alpha.slice(0,26);
+var lowerAlpha = pwdLetters.slice(26,52);
+var symbols = '!@#$%^&*)?(';
+var specChars = symbols.split('');
 var userNeeds = prompt("How long should you password be?");
 var userNeedsSpec = confirm("Would you like to include special characters?");
 var includeNumber = confirm("Would you like to include numbers in your password?");
+// var upperCase = confirm("Would you like to include uppercase letters in your password?");
+// var lowerCase = confirm("Would you like to include lowercase letters in your password?");
 
 var randomNumber = Math.floor(Math.random() * pwdLetters.length);
 var passWord = "" 
+
 for (i = 0; i < userNeeds; i++) {
   var randomNumber = Math.floor(Math.random() * pwdLetters.length);  
   var includeRandomNum = Math.floor(Math.random() * 10);
-  var includeSpecChars = Math.floor(Math.random() * pwdChars.length);
+  var includeSpecChars = Math.floor(Math.random() * specChars.length);
   if (passWord.length >= userNeeds) {
     break;
   }
   if (userNeedsSpec == true) {
-    passWord += pwdChars[includeSpecChars];
+    passWord += specChars[includeSpecChars];
       } 
   if (includeNumber){
     passWord += includeRandomNum; 
   }  
-  
   passWord += alpha[randomNumber];
-  passWord = passWord + alpha[randomNumber];
+  // the above line of code is shorthand for the below line of code
+  // passWord = passWord + alpha[randomNumber];
    
 }
 console.log(passWord);
+
+
+
 // for (i = 0; i < userNeeds; i++) {
 //   console.log(alpha[randomNumber]);
 // }
