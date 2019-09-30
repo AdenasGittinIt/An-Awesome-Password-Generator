ff1bae3a228c24cb30872fd82603dd257fdc6f54
Adena McGibbon <justadena@gmail.com>
2019-09-29 22:29:23 -0600

    Added pseudo code. Removed unused JS code.

--- README.md
+++ README.md
@@ -14,15 +14,15 @@ The user will also have the option to click a button to copy the password to the
 Your application should also be responsive, ensuring that it adapts to multiple screen sizes.
 
 
-User Story
+User Story:
 AS AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
 SO THAT I can create a strong password that provides greater security
 
-Business Context
+Business Context:
 For companies that handle large amounts of sensitive data, weak passwords can pose a real security threat. An application that can generate strong passwords quickly and effortlessly saves employees time and ensures secure access to data.
 
-Acceptance Criteria
+Acceptance Criteria:
 GIVEN that a user needs a new, secure password
 WHEN prompted for password criteria
 THEN a password is generated
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//The code written below will generate a password based on the user's criteria but it is not truly random because
-//I'm systematically choosing a random item from each of my individual arrays. 
-//I will work on fixing this and hopefully update GitHub with the result before this is graded.  
+/* The code written below will generate a password based on the user's criteria but it is not truly random because
+I'm systematically choosing a random item from each of my individual arrays. 
+I will work on fixing this and hopefully update GitHub with the result before this is graded. */  
 
 var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 var lettersArray = alpha.split('');
@@ -8,17 +8,21 @@ var upperAlpha = alpha.slice(0,26);
 var lowerAlpha = lettersArray.slice(26,52);
 var symbols = '!@#$%^&*)?(';
 var specChars = symbols.split('');
-var userNeeds = prompt("How many characters would you like your password to be?");
+
+// The pormpts and confirms will be replaced by the users input into a form field or radio button
+var userNeeds = prompt("How many characters would you like your password to be? (must be at least 8 characters)");
+
+// I was trying to use this code block to make sure the user enters a number greater than 7.
+// if (userNeeds < 8 ) {
+//   alert("Strong passwords must be at least 8 characters long.  Please enter a nubmer greater than seven.");
+//   location.reload();
+// }
+
 var userNeedsSpec = confirm("Would you like to include special characters?");
 var userNeedsNum = confirm("Would you like to include numbers in your password?");
 var upperCase = confirm("Would you like to include uppercase letters in your password?");
 var lowerCase = confirm("Would you like to include lowercase letters in your password?");
 var passWord = "" 
-
-if (userNeeds < 8 ) {
-  alert("Strong passwords must be at least 8 characters long");
-  location.reload();
-}
 
 
 for (i = 0; i < userNeeds; i++) {
@@ -56,6 +60,9 @@ for (i = 0; i < userNeeds; i++) {
     break;
   }
  
+  if(lowerCase) {
+    passWord += lowerAlpha[includeLowerCase];
+  }
  
   // if (passWord += alpha[randomNumber]){
 
@@ -70,35 +77,35 @@ for (i = 0; i < userNeeds; i++) {
   // passWord = passWord + alpha[randomNumber];
 }
 
-alert("Your password is " + passWord);
-
 // console.log(passWord);
 // console.log(passWord.length);
 
-
-// Pseudo Code
-
-// Capture the number of characters a user needs (length)
-
-// Capture whether or not the user needs spcecial characters
-
-// Capture whether or not the user needs numbers
-
-// Capture whether the user wants to include lowercase characters
-
-// Capture whether the user wants to include upper characters
-
-// Based on the above criteria return a string 
-
-// I don't want the user to work with prompts and alerts but I will use them as a tool to get my JS functioning properly.
+// This alert will be replaced by a method that copies the user's password to the clipboard
+alert("Your password is " + passWord);
 
 
-// Not all characters can sit in the same array since some are optional.  BUT I can have an arrary of arrays.
+//  Pseudo Code
 
-// I need a method for randomly selecting these items from the array that they live in.
+//  Capture the number of characters a user needs (length)
 
-// I need a means by which I can return the resulting password to the user.  
+//  Capture whether or not the user needs spcecial characters
 
-// I need a button that the user can click that will save the password to their clipboard and a method for doing so.
+//  Capture whether or not the user needs numbers
 
-// What can I do to make this mundane task interesting and a little fun?
+//  Capture whether the user wants to include lowercase characters
+
+//  Capture whether the user wants to include uppercase characters
+
+//  Based on the above criteria creeate and return a string 
+
+//  I don't want the user to work with prompts and alerts but I will use them initially as a tool to get my JS functioning properly.
+
+//  Not all characters can sit in the same array since some are optional.  BUT I can have an arrary of arrays and then randomly select from there?
+
+//  I need a method for randomly selecting these items from the array that they live in.
+
+//  I need a means by which I can return the resulting password to the user.  
+
+//  I need a button that the user can click that will save the password to their clipboard and a method for doing so.
+
+//  What can I do to make this mundane task interesting and a little fun?
