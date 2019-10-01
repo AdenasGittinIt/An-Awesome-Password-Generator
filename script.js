e7a80f34c59ace858683196637ceb728108d936d
Adena McGibbon <justadena@gmail.com>
2019-09-30 23:24:52 -0600

    Added a function that truly randomizes the password but only works if user confirms all options.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-/* The code written below will generate a password based on the user's criteria but it is not truly random because
-I'm systematically choosing a random item from each of my individual arrays. 
-I will work on fixing this and hopefully update GitHub with the result before this is graded. */  
-
 var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 var lettersArray = alpha.split('');
 var upperAlpha = alpha.slice(0,26);
 var lowerAlpha = lettersArray.slice(26,52);
 var symbols = '!@#$%^&*)?(';
 var specChars = symbols.split('');
+var numbers = '123456789'
+var numbersArray = numbers.split('');
+var userOptions = [upperAlpha, numbersArray, specChars, lowerAlpha];
+
 
 // The pormpts and confirms will be replaced by the users input into a form field or radio button
 var userNeeds = prompt("How many characters would you like your password to be? (must be at least 8 characters)");
 
-// I was trying to use this code block to make sure the user enters a number greater than 7.
+// I was trying to use this code block to make sure the user enters a number greater than 7 but it didn't quite get me 
+// where I wanted to go
+
 // if (userNeeds < 8 ) {
 //   alert("Strong passwords must be at least 8 characters long.  Please enter a nubmer greater than seven.");
 //   location.reload();
@@ -24,61 +26,61 @@ var upperCase = confirm("Would you like to include uppercase letters in your pas
 var lowerCase = confirm("Would you like to include lowercase letters in your password?");
 var passWord = "" 
 
+function getRandom(arr) {
+  var random1 = Math.floor((Math.random() * (arr.length)));
+  return arr[random1][Math.floor((Math.random() * (arr[random1].length)))];
+}
 
 for (i = 0; i < userNeeds; i++) {
-  var randomNumber = Math.floor(Math.random() * lettersArray.length);  
-  var includeRandomNum = Math.floor(Math.random() * 10);
+  // var randomNumber = Math.floor(Math.random() * lettersArray.length);  
+  var includeRandomNum = Math.floor(Math.random() * numbersArray.length);
   var includeSpecChars = Math.floor(Math.random() * specChars.length);
   var includeUpperCase = Math.floor(Math.random() * upperAlpha.length);
   var includeLowerCase = Math.floor(Math.random() * lowerAlpha.length);
 
-  if (passWord.length >= userNeeds) {
-    break;
-  }
-
-  if (userNeedsSpec) {
-    passWord += specChars[includeSpecChars];
-  } 
-  console.log(passWord);
-  if (passWord.length >= userNeeds) {
-    break;
-  }
-  
-  if (userNeedsNum){
-    passWord += includeRandomNum; 
-  }  
-  console.log(passWord);
-  if (passWord.length >= userNeeds) {
-    break;
-  }
-
-  if (upperCase) {
-    passWord += upperAlpha[includeUpperCase];
-  }
-  console.log(passWord);
-  if (passWord.length >= userNeeds) {
-    break;
-  }
- 
-  if(lowerCase) {
-    passWord += lowerAlpha[includeLowerCase];
-  }
- 
-  // if (passWord += alpha[randomNumber]){
-
-  // };
 
   if (passWord.length >= userNeeds) {
     break;
   }
-  
-  //passWord += alpha[randomNumber];
-  // the above line of code is shorthand for the below line of code
-  // passWord = passWord + alpha[randomNumber];
+
+if (userNeedsSpec && userNeedsNum && upperCase && lowerCase){
+  passWord += getRandom(userOptions);
 }
 
-// console.log(passWord);
-// console.log(passWord.length);
+  // if (userNeedsSpec) {
+  //   passWord += specChars[includeSpecChars];
+  // } 
+  // console.log(passWord);
+  // if (passWord.length >= userNeeds) {
+  //   break;
+  // }
+  
+  // if (userNeedsNum){
+  //   passWord += includeRandomNum; 
+  // }  
+  // console.log(passWord);
+  // if (passWord.length >= userNeeds) {
+  //   break;
+  // }
+
+  // if (upperCase) {
+  //   passWord += upperAlpha[includeUpperCase];
+  // }
+  // console.log(passWord);
+  // if (passWord.length >= userNeeds) {
+  //   break;
+  // }
+ 
+  // if(lowerCase) {
+  //   passWord += lowerAlpha[includeLowerCase];
+  // }
+ 
+
+  // if (passWord.length >= userNeeds) {
+  //   break;
+  // }
+
+}
 
 // This alert will be replaced by a method that copies the user's password to the clipboard
 alert("Your password is " + passWord);
@@ -109,3 +111,8 @@ alert("Your password is " + passWord);
 //  I need a button that the user can click that will save the password to their clipboard and a method for doing so.
 
 //  What can I do to make this mundane task interesting and a little fun?
+
+// My random notes...
+  //passWord += alpha[randomNumber];
+  // the above line of code is shorthand for the below line of code
+  // passWord = passWord + alpha[randomNumber];
